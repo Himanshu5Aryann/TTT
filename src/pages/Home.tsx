a6bb3f713a9170418c25921d3baa62d9c67cbcb9
Himanshu Aryann <himanshuaaryan887@gmail.com>
2026-09-25 14:09:06 +0530

    Add files via upload

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import Hero from "../components/Hero";
+import CredibilityBar from "../components/CredibilityBar";
+import BrandStatement from "../components/BrandStatement";
+import FeaturedProjects from "../components/FeaturedProjects";
+import ProjectSpotlight from "../components/ProjectSpotlight";
+import Transformation from "../components/Transformation";
+import Testimonials from "../components/Testimonials";
+import Services from "../components/Services";
+import WhyChooseUs from "../components/WhyChooseUs";
+import ProcessTimeline from "../components/ProcessTimeline";
+import AboutStudio from "../components/AboutStudio";
+import FAQ from "../components/FAQ";
+import FinalCTA from "../components/FinalCTA";
+import ContactSection from "../components/ContactSection";
+import MapSection from "../components/MapSection";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <AboutStudio />
+      <CredibilityBar />
+      <BrandStatement />
+      <FeaturedProjects />
+      <ProjectSpotlight />
+      <Transformation />
+      <Testimonials />
+      <Services />
+      <WhyChooseUs />
+      <ProcessTimeline />
+      <FAQ />
+      <FinalCTA />
+      <ContactSection />
+      <MapSection />
+    </>
+  );
+}
